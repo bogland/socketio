bf306bb9c4acc6bccd5984194c3a22309f089878
unknown <guruwang2@gmail.com>
2021-09-20 21:36:09 +0900

    socketio 연동중

--- src/server.js
+++ src/server.js
@@ -1,15 +1,28 @@
 import express from "express";
+import http from "http";
+import SocketIO from "socket.io";
 
 const app = express();
-
 app.set('view engine',"pug");
 app.set("views",__dirname + "/views");
 app.use("/public",express.static(__dirname + "/public"));
 
-app.get("/",(req,res)=>{
-    res.render("home");
-});
+app.get("/",(_,res)=>
+    res.render("home")
+);
+app.get("/*",(_,res)=>res.redirect("/"));
+
+const server = http.createServer(app);
+const io = SocketIO(server);
+
+io.on("connection",socket=>{
+    console.log(socket);
+})
 
 app.listen(3000,()=>{
     console.log("start express server on port 3000")
 });
+
+
+
+
